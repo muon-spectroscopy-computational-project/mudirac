cf168cbb9e0db16a6d7ede592755b4d953332320
stur86 <simonesturniolo@gmail.com>
2019-04-05 18:23:16 +0100

    Switched to CMake

--- test/data/genlag.py
+++ test/data/genlag.py
@@ -4,23 +4,32 @@ import os
 import numpy as np
 from scipy.special import genlaguerre
 
-header = ''
+lag_params = '{'
 
+min_n = 1
+max_n = 3
 x = np.linspace(0, 5)
+
 lvals = []
-for n in range(1, 3):
+for n in range(min_n, max_n):
     for alpha in range(0, n):
-        header += '# {0} {1}\n'.format(n, alpha)
+        lag_params += '{{ {0}, {1} }},\n'.format(n, alpha)
         lp = genlaguerre(n, alpha)
         lvals.append(lp(x))
 
+lag_params += '}'
+lag_values = '{'
+
 lvals = np.array(lvals)
 
-fout = open('{0}.dat'.format(os.path.splitext(__file__)[0]), 'w')
-fout.write(header)
-fout.write('## END HEADER ## \n')
 for i in range(0, len(x)):
-    fout.write('{0} {1}\n'.format(x[i], ' '.join(map(str, lvals[:,i]))))
-fout.close()
+    lag_values += '{{ {0}, {1} }},\n'.format(x[i], ', '.join(map(str,
+                                                                 lvals[:, i])))
+lag_values += '}'
 
-
+path = os.path.split(__file__)[0]
+with open(os.path.join(path, 'genlag.h'), 'w') as f:
+    f.write('int laguerreParams[{0}][2] = '.format(
+        lvals.shape[0]) + lag_params + ';\n')
+    f.write('double laguerreValues[{0}][{1}] = '.format(
+        lvals.shape[1], lvals.shape[0]+1) + lag_values + ';')
--- test/test_utils.cpp
+++ test/test_utils.cpp
@@ -7,6 +7,7 @@
 #include <fstream>
 #include <iostream>
 #include "../src/utils.hpp"
+#include "data/genlag.h"
 
 #include "catch/catch.hpp"
 
@@ -15,48 +16,30 @@ bool compareLaguerre(double tol = 1e-5)
     // Compare Laguerre polynomials with reference table
 
     int val;
+    int pN, xN;
     string tag, error;
-    vector<int> n, alpha;
 
+    int n, alpha;
     double x, ydat, ycalc;
 
-    ifstream datFile;
-
-    datFile.open("test/data/genlag.dat");
-
-    if (!datFile)
-    {
-        throw "Reference table not found.";
-    }
-
-    // Read the header
-
-    while (datFile >> tag && tag.compare("##"))
-    {
-        datFile >> val;
-        n.push_back(val);
-        datFile >> val;
-        alpha.push_back(val);
-    }
-    datFile.ignore(100, '\n');
-
-    if (n.size() == 0)
-    {
-        throw "No parameters found in reference table.";
-    }
+    pN = sizeof(laguerreParams) / (2 * sizeof(int));
+    xN = sizeof(laguerreValues) / ((pN + 1) * sizeof(double));
 
     // Now read everything else
-    while (datFile >> x)
+    for (int i = 0; i < xN; ++i)
     {
-        for (int i = 0; i < n.size(); ++i)
+        x = laguerreValues[i][0];
+        for (int j = 0; j < pN; ++j)
         {
-            datFile >> ydat;
-            ycalc = genLaguerrePoly(x, n[i], alpha[i]);
+            n = laguerreParams[j][0];
+            alpha = laguerreParams[j][1];
+            ydat = laguerreValues[i][j + 1];
+            ycalc = genLaguerrePoly(x, n, alpha);
             if (abs(ydat - ycalc) > tol)
             {
                 throw("Polynomials don't match for x = " + to_string(x) +
-                      " n = " + to_string(n[i]) +
-                      " alpha = " + to_string(alpha[i]));
+                      " n = " + to_string(n) +
+                      " alpha = " + to_string(alpha));
             }
         }
     }
