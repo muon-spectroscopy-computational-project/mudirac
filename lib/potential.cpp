0d5c2503dde820e9e46e44b98ea064cb3cd2c6a6
Simone Sturniolo <simone.sturniolo@gmail.com>
2019-05-01 17:24:46 +0100

    Added Potential files and classes

---
+++ lib/potential.cpp
@@ -0,0 +1,36 @@
+/**
+ * MuDirac - A muonic atom Dirac equation solver
+ * by Simone Sturniolo (2019)
+ * 
+ * potential.hpp
+ * 
+ * Classes for various types of potentials
+ * 
+ * @author Simone Sturniolo
+ * @version 0.1 04/02/2019
+ */
+
+#include "potential.hpp"
+
+CoulombPotential::CoulombPotential(double Z, double R)
+{
+    this->Z = Z;
+    this->R = R;
+    R3 = pow(R, 3);
+    this->VR = R > 0 ? -1.5 * Z / R : 0;
+}
+
+double CoulombPotential::V(double r)
+{
+    if (r < 0)
+    {
+        throw invalid_argument("Negative radius not allowed for CoulombPotential");
+    }
+    else if (r < R)
+    {
+        return Z * pow(r, 2) / (2 * R3) + VR;
+    }
+    else {
+        return -Z/r;
+    }
+}
---
+++ lib/potential.hpp
@@ -0,0 +1,34 @@
+/**
+ * MuDirac - A muonic atom Dirac equation solver
+ * by Simone Sturniolo (2019)
+ * 
+ * potential.hpp
+ * 
+ * Classes for various types of potentials - header file
+ * 
+ * @author Simone Sturniolo
+ * @version 0.1 04/02/2019
+ */
+
+#include <cmath>
+#include <vector>
+#include <stdexcept>
+#include "integrate.hpp"
+
+using namespace std;
+
+class Potential
+{
+public:
+    virtual double V(double r);
+};
+
+class CoulombPotential : Potential
+{
+public:
+    CoulombPotential(double Z, double R = -1);
+    double V(double r) override;
+
+protected:
+    double R, R3, VR, Z;
+};
