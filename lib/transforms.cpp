3ea7bbb64bc8ce4a12d9da6988d63e099236d96f
Simone Sturniolo <simone.sturniolo@stfc.ac.uk>
2019-05-02 15:31:49 +0100

    Added transforms.cpp

---
+++ lib/transforms.cpp
@@ -0,0 +1,11 @@
+/**
+ * MuDirac - A muonic atom Dirac equation solver
+ * by Simone Sturniolo (2019)
+ * 
+ * transforms.hpp
+ * 
+ * Functions to perform various kinds of function transforms
+ * 
+ * @author Simone Sturniolo
+ * @version 0.1 04/02/2019
+ */
