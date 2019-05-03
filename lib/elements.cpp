ec7fc104b58b834975acc1475d6755c421b92bf2
Simone Sturniolo <simone.sturniolo@stfc.ac.uk>
2019-05-03 15:42:04 +0100

    Created elements.cpp

---
+++ lib/elements.cpp
@@ -0,0 +1,27 @@
+/**
+ * MuDirac - A muonic atom Dirac equation solver
+ * by Simone Sturniolo (2019)
+ * 
+ * elements.hpp
+ * 
+ * Atomic mass and spin data extracted by AME2016 and NUBASE databases.
+ * References:
+ * 
+ * AME2016 (masses):
+ *      "The Ame2016 atomic mass evaluation (I)"   by W.J.Huang, G.Audi, M.Wang, F.G.Kondev, S.Naimi and X.Xu
+ *         Chinese Physics C41 030002, March 2017.
+ *      "The Ame2016 atomic mass evaluation (II)"  by M.Wang, G.Audi, F.G.Kondev, W.J.Huang, S.Naimi and X.Xu
+ *         Chinese Physics C41 030003, March 2017.
+ * 
+ * NUBASE (spins):
+ *      "The NUBASE2016 evaluation of nuclear properties"   by G.Audi, F.G.Kondev, M.Wang, W.J.Huang and S.Naimi
+ *         Chinese Physics C41 030001, March 2017.
+ * 
+ * @author Simone Sturniolo
+ * @version 0.1 04/02/2019
+ */
+
+#include "elements.hpp"
+
+
+
