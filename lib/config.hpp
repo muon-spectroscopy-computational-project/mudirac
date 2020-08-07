6310d799335d35beb67104821175d1cacf4198ba
stur86 <simonesturniolo@gmail.com>
2020-08-07 17:14:55 +0100

    Added FERMI2 option to input files

--- lib/config.hpp
+++ lib/config.hpp
@@ -1,21 +1,22 @@
 /**
  * MuDirac - A muonic atom Dirac equation solver
  * by Simone Sturniolo (2019-2020)
- * 
+ *
  * config.hpp
- * 
- * Functions and classes useful for interpreting configuration files - header file
- * 
+ *
+ * Functions and classes useful for interpreting configuration files - header
+ * file
+ *
  * @author Simone Sturniolo
  * @version 1.0 20/03/2020
  */
 
-#include <vector>
-#include <map>
-#include "input.hpp"
 #include "atom.hpp"
-#include "elements.hpp"
 #include "econfigs.hpp"
+#include "elements.hpp"
+#include "input.hpp"
+#include <map>
+#include <vector>
 
 #include "../vendor/aixlog/aixlog.hpp"
 
@@ -25,23 +26,22 @@ using namespace std;
 #define MUDIRAC_CONFIG
 
 /**
-  * @class MuDiracInputFile
-  * 
-  * @brief  A specialised InputFile class for the config input file in mudirac
-  * @note   A specialised InputFile class for the config input file in mudirac.
-  * Initialises all the relevant keywords.
-  * 
-  * @retval None
-  */
-class MuDiracInputFile : public InputFile
-{
+ * @class MuDiracInputFile
+ *
+ * @brief  A specialised InputFile class for the config input file in mudirac
+ * @note   A specialised InputFile class for the config input file in mudirac.
+ * Initialises all the relevant keywords.
+ *
+ * @retval None
+ */
+class MuDiracInputFile : public InputFile {
 public:
   MuDiracInputFile(void);
   DiracAtom makeAtom();
 
 private:
-  map<string, NuclearRadiusModel> nucmodelmap = {{"POINT", POINT},
-                                                 {"SPHERE", SPHERE}};
+  map<string, NuclearRadiusModel> nucmodelmap = {
+      {"POINT", POINT}, {"SPHERE", SPHERE}, {"FERMI2", FERMI2}};
 };
 
 #endif
