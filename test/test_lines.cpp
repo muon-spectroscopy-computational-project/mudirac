6263d406c817800ebe0fb9df07fc1b7be77f37b3
Subindev D <64056430+subindev-d@users.noreply.github.com>
2024-10-04 16:09:47 +0100

    Fixed line test
    introudcing the the newparmeter radius breaks the test_lines

--- test/test_lines.cpp
+++ test/test_lines.cpp
@@ -21,14 +21,14 @@ Table VI
 */
 
 vector<double> energyCorrections(int Z, int n1, int l1, bool s1, int n2, int l2,
-                                 bool s2, NuclearRadiusModel rmodel = SPHERE)
+                                 bool s2, NuclearRadiusModel rmodel = SPHERE, double radius= -1)
 {
   vector<double> E_corr;
   DiracAtom da_point = DiracAtom(Z, Physical::m_mu, getElementMainIsotope(Z));
   DiracAtom da_sphere =
-      DiracAtom(Z, Physical::m_mu, getElementMainIsotope(Z), rmodel);
+      DiracAtom(Z, Physical::m_mu, getElementMainIsotope(Z), radius, rmodel);
   DiracAtom da_uehling =
-      DiracAtom(Z, Physical::m_mu, getElementMainIsotope(Z), rmodel);
+      DiracAtom(Z, Physical::m_mu, getElementMainIsotope(Z), radius, rmodel);
   da_uehling.setUehling(true, 100);
 
   double E_point = da_point.getState(n2, l2, s2).E;
