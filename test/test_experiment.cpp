9d53d0f631a43d3bd520c6f47dd779c96775184c
MilanSCD <milan.kumar@stfc.ac.uk>
2025-03-11 10:33:10 +0000

    add normal use test for 2pF parameterisation

--- test/test_experiment.cpp
+++ test/test_experiment.cpp
@@ -27,7 +27,7 @@ TEST_CASE("test fermi parameterisation 0 ", "[fermiParameters]")
     rms_radius = rmsRadius(output_c, output_t);
     REQUIRE( output_c == Approx(0).epsilon(0.01) );
     REQUIRE(output_t == Approx(0.0).epsilon(0.01));
-    REQUIRE(rms_radius == Approx(0.0).margin(0.0001));
+    REQUIRE(rms_radius == Approx(0.0).margin(0.00001));
 }
 
 TEST_CASE("test fermi parameterisation t", "[fermiParameters]")
@@ -35,7 +35,17 @@ TEST_CASE("test fermi parameterisation t", "[fermiParameters]")
     double output_c, output_t, rms_radius;
     tie(output_c, output_t) = fermiParameters(5.88, M_PI/2);
     rms_radius = rmsRadius(output_c, output_t);
-    REQUIRE( output_c == Approx(0.0).margin(0.001));
+    REQUIRE( output_c == Approx(0.0).margin(0.00001));
     REQUIRE(output_t == Approx(6.95).epsilon(0.01));
-    REQUIRE(rms_radius == Approx(5.88).margin(0.0001));
+    REQUIRE(rms_radius == Approx(5.88).margin(0.00001));
+}
+
+TEST_CASE("test fermi parameterisation normal use", "[fermiParameters]")
+{
+    double output_c, output_t, rms_radius;
+    tie(output_c, output_t) = fermiParameters(5.88, M_PI/12);
+    rms_radius = rmsRadius(output_c, output_t);
+    REQUIRE( output_c == Approx(7.285559152).margin(0.00001));
+    REQUIRE(output_t == Approx(1.952159691).margin(0.00001));
+    REQUIRE(rms_radius == Approx(5.88).margin(0.00001));
 }
