5d3a0f1907bbd90b2fad5ffd24267a55918e4952
MilanSCD <milan.kumar@stfc.ac.uk>
2025-06-06 16:29:15 +0100

    remove unused readXRayMeasurements function

--- lib/experiment.hpp
+++ lib/experiment.hpp
@@ -40,6 +40,7 @@ class ExperimentalResultFile : public InputFile {
 // a constant in the ellipse equation containing fermi_c, fermi_t and uniform radius R
 const double ellipse_const = (7*M_PI*M_PI)/(48*log(3)*log(3));
 
+
 /**
  *
  *
@@ -53,7 +54,6 @@ const double ellipse_const = (7*M_PI*M_PI)/(48*log(3)*log(3));
 pair<double, double> fermiParameters(double rms_radius, double theta);
 
 
-
 struct OptimisationData {
   double rms_radius;
   double theta;
@@ -61,27 +61,5 @@ struct OptimisationData {
   double fermi_c;
   double fermi_t;
 };
-
-
-/**
- *
- *
- * @brief  A function to read the experimental measurements input file
- * and store the data in referenced variables
- * @note   A function which reads the experimental measurements input file
- * into the referenced xr_measurements object. The data of xr_lines, energies errors
- * are copied into referenced vector variables. if successful the references read_success
- * variable is true so the main function can continue with 2 parameter fermi optimisation.
- *
- * @param xr_measurements:    object to contain data from xr input file
- * @param read_success:     switch to control whether 2pF optimisation should occur
- * @param xr_lines:     list of measured xray transitions
- * @param xr_energies:      list of measured xray energies
- * @param xr_errors:      list of measured xray errors
- * @param argv:      MuDirac argument containing the experimental input file
- *
- */
-void readXrayMeasurements(ExperimentalResultFile &xr_measurements, bool &read_success, vector<string> &xr_lines, vector<double> &xr_energies, vector<double> &xr_errors, char *argv[]);
-
 
 #endif
